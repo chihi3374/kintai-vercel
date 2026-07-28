31ac94674f384f1c6d061c52c1f071279bd9cafd
chihi3374 <chihi3374@gmail.com>
2026-07-29 02:54:15 +0900

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,15 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+
 console.log("DEBUG: CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("DEBUG: CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET ? "読み込み成功" : "読み込み失敗！");
+console.log(
+  "DEBUG: CLIENT_SECRET:",
+  process.env.GOOGLE_CLIENT_SECRET ? "読み込み成功" : "読み込み失敗！"
+);
+
 export const authOptions: NextAuthOptions = {
+  debug: true,
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
@@ -10,15 +17,18 @@ export const authOptions: NextAuthOptions = {
       authorization: {
         params: {
           // ★ここが超重要！ログイン時にスプレッドシート自動作成のための権限を要求します
-          scope: "openid email profile https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/spreadsheets",
+          scope:
+            "openid email profile https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/spreadsheets",
           prompt: "select_account",
           access_type: "offline",
-          response_type: "code"
-        }
-      }
-    })
+          response_type: "code",
+        },
+      },
+    }),
   ],
+
   useSecureCookies: true,
+
   callbacks: {
     // ログイン成功時にGoogleから返ってきた「アクセスキー」をプログラム内で使えるように保持します
     async jwt({ token, account }) {
@@ -27,14 +37,17 @@ export const authOptions: NextAuthOptions = {
       }
       return token;
     },
+
     async session({ session, token }) {
       // セッション情報にトークンをねじ込みます
       (session as any).accessToken = token.accessToken;
       return session;
-    }
+    },
   },
+
   secret: process.env.NEXTAUTH_SECRET,
 };
 
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
